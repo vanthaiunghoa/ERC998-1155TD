d776ba3ea54775e65954331cde40112a6e2da0ca
ivan <ivanplociennik.dev@protonmail.com>
2021-07-02 12:05:16 +0200

    REM useless code

--- contracts/testERC1155.sol
+++ contracts/testERC1155.sol
@@ -1,22 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract testERC1155 is ERC1155 {
-    uint256 public constant GOLD = 0;
-    uint256 public constant SILVER = 1;
-    uint256 public constant THORS_HAMMER = 2;
-    uint256 public constant SWORD = 3;
-    uint256 public constant SHIELD = 4;
-
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
-     
-    }
+    constructor() ERC1155("https://game.example/api/item/{id}.json") {}
 
     // FOR TESTING
     function mint(address _to, uint256 _tokenID) public {
         _mint(_to, _tokenID, 1, "");
+    }
+    function mintBatch(address _to, uint256[] memory _ids, uint256[] memory _amounts, bytes memory _data) public {
+        _mintBatch(_to, _ids, _amounts, _data);
     }
 }
